c00b98fee4b99fd7cbaed2ab223fecea980008f9
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-12 19:27:52 -0500

    Prompt written.

---
+++ src/java_prg_05_18_esp_game/JAVA_PRG_05_18_ESP_Game.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_05_18_esp_game;
+
+/**
+ *
+ * @author bluebackdev
+ * Write a program that tests your ESP (extrasensory perception). The program
+ * should randomly select the name of a color from the following list of words:
+ * 
+ *      Red, Green, Blue, Orange, Yellow
+ * 
+ * To select a word, the program can generate a random number. For example, if
+ * the number is 0, the selected word is Red; if the number is 1, the selected
+ * word is Green; and so forth.
+ * 
+ * Next, the program should ask the user to enter the color that the computer
+ * has selected. After the user has entered his or her guess, the program should
+ * display the name of the randomly selected color. The program should repeat
+ * this 10 times and then display the number of times the user correctly
+ * guessed the selected color. Be sure to modularize the program into methods
+ * that perform each major task.
+ */
+public class JAVA_PRG_05_18_ESP_Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
